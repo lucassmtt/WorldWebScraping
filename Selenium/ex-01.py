ed32ff25730c6f409ba1ed299af66a37ca192b58
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-03-10 23:45:13 -0300

    Update ex-01.py

--- Selenium/ex-01.py
+++ Selenium/ex-01.py
@@ -1,11 +1,16 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+"""
+OBS: é importante lembrar que em versões mais recentes do Selenium
+temos que importar o "By" 
+"""
 
 chrome = Chrome()
-chrome.get('https://github.com/lucassmtt')
+chrome.get('https://github.com/lucassmtt')#busca a url
 
-my_search = chrome.find_elements(By.TAG_NAME, 'div')
+my_search = chrome.find_elements(By.TAG_NAME, 'div')#faz uma pesquisa usando
+                                                    #o By(por nome de tag)
 
-for i in my_search:
+for i in my_search: #looping na minha pesquisa
     print(i.text)
 
