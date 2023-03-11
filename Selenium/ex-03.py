f59b88bd9411764f3600767d120ffa1b66ab55eb
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-03-11 01:00:37 -0300

    Update ex-03.py

--- Selenium/ex-03.py
+++ Selenium/ex-03.py
@@ -16,16 +16,41 @@ from selenium.webdriver.common import by
 
 
 def search_products(url: str) -> [str]:
-    my_links = list()
+    my_links = {}
     browser = Chrome()
 
     browser.get(url)
     search = browser.find_elements(by.By.TAG_NAME, 'a')
 
     for links in search:
-        my_links.append(links.get_attribute('href'))
+        my_links.setdefault(f'{links.text}',
+                            f'{links.get_attribute("href")}')
 
     return my_links
 
-print(search_products('https://templodela.com'))
 
+def search_collections(url: str, number: int, options: str) -> [str]:
+    """
+    URL: url search
+    NUMBER: 1 == return link of collection
+            2 == return search of products in links of collections
+    OPTIONS: options of filters
+    """
+
+    my_links = {}
+    browser = Chrome()
+
+    browser.get(url)
+    search = browser.find_elements(by.By.TAG_NAME, 'a')
+
+    for links in search:
+        if links.text.lower() == options:
+            links_collections = links.get_attribute('href')
+
+            if number == 1:
+                return links_collections
+            else:
+                return search_products(links_collections)
+
+my_search = search_collections('https://templodela.com', 2, 'ENTRAR EM CONTATO')
+print(my_search)
