6b70592f8374f7fa91cea5231e8d33d201ca0d12
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-03-11 00:19:24 -0300

    Criando função que raspe e pegue links.

--- Selenium/ex-03.py
+++ Selenium/ex-03.py
@@ -1,14 +1,31 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common import by
-my_browser = Chrome()
-my_browser.get('https://selenium.dunossauro.live/aula_04_a.html')
 
-search = my_browser.find_element(by.By.TAG_NAME, 'ul') #pega somente a primeira
-                                                       #aparição do ul
-print(search.text)
-print('-'*30)
 
-other_search = my_browser.find_elements(by.By.TAG_NAME, 'ul')#pega todas as aparições
-for items in other_search:
-    print(items.text)
+# my_browser = Chrome()
+# my_browser.get('https://templodela.com')
+#
+# search = my_browser.find_element(by.By.TAG_NAME, 'ul') #pega somente a primeira
+#                                                        #aparição do ul
+# print(search.text)
+# print('-'*30)
+#
+# other_search = my_browser.find_elements(by.By.TAG_NAME, 'a')#pega todas as aparições
+# for items in other_search:
+#     print(items.get_attribute('href'))
+
+
+def search_products(url: str) -> [str]:
+    my_links = list()
+    browser = Chrome()
+
+    browser.get(url)
+    search = browser.find_elements(by.By.TAG_NAME, 'a')
+
+    for links in search:
+        my_links.append(links.get_attribute('href'))
+
+    return my_links
+
+print(search_products('https://templodela.com'))
 
