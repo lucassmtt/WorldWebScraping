83d5d0011b6b4488a84258c5d496ae0a0f023042
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-03-15 14:16:03 -0300

    Update ex-06..py

--- Selenium/ex-06..py
+++ Selenium/ex-06..py
@@ -1,6 +1,9 @@
-from selenium.webdriver import Chrome
-from selenium.webdriver.common.by import By
+# from selenium.webdriver import Chrome
+# from selenium.webdriver.common.by import By
+#
+# browser = Chrome()
+# browser.get('http://selenium.dunussauro.live/aula_04_b.html')
+# browser.find_element(By.TAG_NAME, '')
 
-browser = Chrome()
-browser.get('http://selenium.dunussauro.live/aula_04_b.html')
-browser.find_element(By.TAG_NAME, '')
+for i in range(1, 5):
+    print(i)
