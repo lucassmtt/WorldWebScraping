ff6c130f145d0b041576bafbe42d5a0354725188
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-03-23 15:24:45 -0300

    Learning about XPATH reading its advantages...

---
+++ Selenium/ex-08_Learning_about_XPATH.py
@@ -0,0 +1,8 @@
+"""
+/html/title : Traz a primeira ocorrência seguindo a árvore
+//title : Traz a primeira ocorrências ignorando a árvore
+ul/* : Traz qualquer tag que vier abaixo
+ul//* : Vai retornar todas as tags que vier abaixo, inclusive aninhadas
+(ul//*)[2] : Vai pegar a expressão inteira e selecionar a segunda ocorrência
+
+"""
