9343b9db62d14ae2271399c73602ac84eadf3893
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-03-31 15:30:28 -0300

    Update ex-08_Learning_about_XPATH.py

--- Selenium/ex-08_Learning_about_XPATH.py
+++ Selenium/ex-08_Learning_about_XPATH.py
@@ -1,3 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from time import sleep
+from pprint import pprint
+
+
 """
 /html/title : Traz a primeira ocorrência seguindo a árvore
 //title : Traz a primeira ocorrências ignorando a árvore
@@ -6,3 +13,11 @@ ul//* : Vai retornar todas as tags que vier abaixo, inclusive aninhadas
 (ul//*)[2] : Vai pegar a expressão inteira e selecionar a segunda ocorrência
 
 """
+
+
+url = 'https://rennerocha.github.io/xpath/'
+browser = Chrome()
+browser.get(url)
+sleep(2)
+element = browser.find_element(By.XPATH, "/html")
+pprint(element.text)
