e66ac3c94b5d1cddcbee12808c0a4aaee647f114
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-04-03 17:55:26 -0300

    Create ex-010_Combiners.py

---
+++ Selenium/ex-010_Combiners.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Chrome
+
+"""
+Testar o CSS_SELECTOR com: "https://flukeout.github.io/"
+E fazer os exercícios 
+"""
+
+browser = Chrome()
+url_exercicio_1 = "https://selenium.dunossauro.live/aula_05.html"
+url_exercicio_2 = "https://selenium.dunossauro.live/aula_06.html"
+url = "https://selenium.dunossauro.live/aula_06_a.html"
+
+browser.get(url)
+
+
