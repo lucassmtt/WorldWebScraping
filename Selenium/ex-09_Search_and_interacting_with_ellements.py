87191d629bd64c8d103c5ebbd65025ffd6bec9a0
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-04-03 16:58:27 -0300

    Aprendendo a utilizar atributo-operador-valor para encontrar elementos no HTML.

--- Selenium/ex-09_Search_and_interacting_with_ellements.py
+++ Selenium/ex-09_Search_and_interacting_with_ellements.py
@@ -10,3 +10,40 @@ my_browser.get(url)
 
 # my_search = my_browser.find_element(By.CSS_SELECTOR, "form")
 # pprint(my_search.text)
+
+pesquisa = my_browser.find_elements(By.CSS_SELECTOR, "[for]")
+
+"""
+[atributo =  valor] : Deve ser exatamente igual
+[atributo *= valor] : Deve ocorrer em
+[atributo |= valor] : Deve ser exatamente ou iniciar
+[atributo ^= valor] : Iniciado em
+[atributo $= valor] : Terminado em
+[atributo ~= valor] : Um deve ser exatamente igual
+"""
+
+pesquisa_1 = my_browser.find_elements(
+    By.CSS_SELECTOR, "[class='form-group']"
+)# Todos atributos CLASS em que o valor seja exatamente igual a FORM-GROUP
+
+pesquisa_2 = my_browser.find_elements(
+    By.CSS_SELECTOR, "[class*='group']"
+)# Todos atributos CLASS em que a palavra GROUP exista
+
+pesquisa_3 = my_browser.find_elements(
+    By.CSS_SELECTOR, "[class|='no']"
+)# Deve ser exatamente ou iniciar com "no" -> nome
+
+pesquisa_4 = my_browser.find_elements(
+    By.CSS_SELECTOR, "[class^='fo']"
+)# Deve ser iniciada com "fo" -> form
+
+pesquisa_5 = my_browser.find_elements(
+    By.CSS_SELECTOR, "[type$='t']"
+)# Todos atributos type que terminam com a letra "t"
+
+pesquisa_6 = my_browser.find_elements(
+    By.CSS_SELECTOR, "[~=]"
+)# Um deve ser exatamente igual
+
+
